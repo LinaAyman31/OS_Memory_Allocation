f043380aa7fae864783ac2bea9be052f6ea2bf46
Luai Anwar <55032660+LoayAnwar@users.noreply.github.com>
2021-05-30 00:39:24 +0200

    UI init

--- OS_Memory_Allocation_Using_Segmentation/mainwindow.cpp
+++ OS_Memory_Allocation_Using_Segmentation/mainwindow.cpp
@@ -1,15 +1,31 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+QStringList row_labels;
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-}
+    ui->tableWidget->hide();
+    QStringList column_labels;
+    column_labels << "Starting Address" << "Size";
+    ui->tableWidget->setColumnCount(2);
+    ui->tableWidget->setHorizontalHeaderLabels(column_labels);
 
+}
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->tableWidget->show();
+   ui->tableWidget->setVerticalHeaderLabels(row_labels);
+   ui->pushButton->setText("Add new hole");
+   ui->tableWidget->setRowCount(ui->tableWidget->rowCount() + 1);
+   row_labels << QString::number(ui->tableWidget->rowCount());
+   ui->tableWidget->setVerticalHeaderLabels(row_labels);
+}
--- build-OS_Memory_Allocation_Using_Segmentation-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build-OS_Memory_Allocation_Using_Segmentation-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -11,9 +11,17 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QRadioButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableWidget>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -22,6 +30,24 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QWidget *widget;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QWidget *widget1;
+    QVBoxLayout *verticalLayout_2;
+    QPushButton *pushButton;
+    QTableWidget *tableWidget;
+    QWidget *widget2;
+    QVBoxLayout *verticalLayout_3;
+    QPushButton *pushButton_2;
+    QTableWidget *tableWidget_2;
+    QWidget *widget3;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label_2;
+    QRadioButton *radioButton;
+    QRadioButton *radioButton_2;
+    QRadioButton *radioButton_3;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -29,12 +55,119 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(800, 600);
+        MainWindow->resize(835, 600);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        widget = new QWidget(centralwidget);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(10, 0, 351, 41));
+        horizontalLayout = new QHBoxLayout(widget);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(widget);
+        label->setObjectName(QString::fromUtf8("label"));
+        QFont font;
+        font.setFamily(QString::fromUtf8("Agency FB"));
+        font.setPointSize(14);
+        font.setItalic(false);
+        label->setFont(font);
+        label->setMidLineWidth(0);
+
+        horizontalLayout->addWidget(label);
+
+        lineEdit = new QLineEdit(widget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+
+        horizontalLayout->addWidget(lineEdit);
+
+        widget1 = new QWidget(centralwidget);
+        widget1->setObjectName(QString::fromUtf8("widget1"));
+        widget1->setGeometry(QRect(10, 50, 331, 221));
+        verticalLayout_2 = new QVBoxLayout(widget1);
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        verticalLayout_2->setContentsMargins(0, 0, 0, 0);
+        pushButton = new QPushButton(widget1);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        QFont font1;
+        font1.setFamily(QString::fromUtf8("Arial Narrow"));
+        font1.setPointSize(12);
+        pushButton->setFont(font1);
+
+        verticalLayout_2->addWidget(pushButton);
+
+        tableWidget = new QTableWidget(widget1);
+        tableWidget->setObjectName(QString::fromUtf8("tableWidget"));
+        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(tableWidget->sizePolicy().hasHeightForWidth());
+        tableWidget->setSizePolicy(sizePolicy);
+        tableWidget->setFont(font1);
+        tableWidget->setAcceptDrops(true);
+        tableWidget->setFrameShape(QFrame::StyledPanel);
+        tableWidget->setFrameShadow(QFrame::Sunken);
+        tableWidget->setLineWidth(1);
+        tableWidget->setMidLineWidth(0);
+        tableWidget->setSizeAdjustPolicy(QAbstractScrollArea::AdjustIgnored);
+        tableWidget->setSelectionMode(QAbstractItemView::SingleSelection);
+        tableWidget->setShowGrid(true);
+        tableWidget->setGridStyle(Qt::SolidLine);
+
+        verticalLayout_2->addWidget(tableWidget);
+
+        widget2 = new QWidget(centralwidget);
+        widget2->setObjectName(QString::fromUtf8("widget2"));
+        widget2->setGeometry(QRect(370, 50, 351, 229));
+        verticalLayout_3 = new QVBoxLayout(widget2);
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        verticalLayout_3->setContentsMargins(0, 0, 0, 0);
+        pushButton_2 = new QPushButton(widget2);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+
+        verticalLayout_3->addWidget(pushButton_2);
+
+        tableWidget_2 = new QTableWidget(widget2);
+        tableWidget_2->setObjectName(QString::fromUtf8("tableWidget_2"));
+
+        verticalLayout_3->addWidget(tableWidget_2);
+
+        widget3 = new QWidget(centralwidget);
+        widget3->setObjectName(QString::fromUtf8("widget3"));
+        widget3->setGeometry(QRect(370, 0, 431, 41));
+        horizontalLayout_2 = new QHBoxLayout(widget3);
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
+        label_2 = new QLabel(widget3);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setFont(font);
+
+        horizontalLayout_2->addWidget(label_2);
+
+        radioButton = new QRadioButton(widget3);
+        radioButton->setObjectName(QString::fromUtf8("radioButton"));
+        QFont font2;
+        font2.setFamily(QString::fromUtf8("Agency FB"));
+        font2.setPointSize(14);
+        radioButton->setFont(font2);
+
+        horizontalLayout_2->addWidget(radioButton);
+
+        radioButton_2 = new QRadioButton(widget3);
+        radioButton_2->setObjectName(QString::fromUtf8("radioButton_2"));
+        radioButton_2->setFont(font2);
+
+        horizontalLayout_2->addWidget(radioButton_2);
+
+        radioButton_3 = new QRadioButton(widget3);
+        radioButton_3->setObjectName(QString::fromUtf8("radioButton_3"));
+        radioButton_3->setFont(font2);
+
+        horizontalLayout_2->addWidget(radioButton_3);
+
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 835, 26));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -48,6 +181,13 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Enter total memory size:", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Add Hole", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "Add Process", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "Choose Method:", nullptr));
+        radioButton->setText(QCoreApplication::translate("MainWindow", "First Fit", nullptr));
+        radioButton_2->setText(QCoreApplication::translate("MainWindow", "Best Fit", nullptr));
+        radioButton_3->setText(QCoreApplication::translate("MainWindow", "Worst Fit", nullptr));
     } // retranslateUi
 
 };
